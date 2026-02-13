68635c2d275304d93b6b3c26c8719ecb6a777ab5
Legitmod17 <neshkin2018@yandex.ru>
2026-02-13 14:52:55 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+
+	int num;
+	int num2;
+	int result;
+
+	cout << "Таблица умножения\n";
+	cout << "Введите цифру: ";
+	cin >> num;
+	cout << endl;
+	
+
+	
+
+	cout << "\x1b[92m";
+	switch (num) {
+
+	case 1:
+		cout << "1 * 1 = 1" << endl;
+		cout << "2 * 1 = 2" << endl;
+		cout << "3 * 1 = 3" << endl;
+		cout << "4 * 1 = 4" << endl;
+		cout << "5 * 1 = 5" << endl;
+		cout << "6 * 1 = 6" << endl;
+		cout << "7 * 1 = 7" << endl;
+		cout << "8 * 1 = 8" << endl;
+		cout << "9 * 1 = 9" << endl;
+		cout << "10 * 1 = 10" << endl;
+		return 0;
+	case 2:
+		cout << "2 * 1 = 2" << endl;
+		cout << "2 * 2 = 4" << endl;
+		cout << "2 * 3 = 6" << endl;
+		cout << "2 * 4 = 8" << endl;
+		cout << "2 * 5 = 10" << endl;
+		cout << "2 * 6 = 12" << endl;
+		cout << "2 * 7 = 14" << endl;
+		cout << "2 * 8 = 16" << endl;
+		cout << "2 * 9 = 18" << endl;
+		cout << "2 * 10 = 20" << endl;
+		return 0;
+	case 3:
+		cout << "3 * 1 = 3" << endl;
+		cout << "3 * 2 = 6" << endl;
+		cout << "3 * 3 = 9" << endl;
+		cout << "3 * 4 = 12" << endl;
+		cout << "3 * 5 = 15" << endl;
+		cout << "3 * 6 = 18" << endl;
+		cout << "3 * 7 = 21" << endl;
+		cout << "3 * 8 = 24" << endl;
+		cout << "3 * 9 = 27" << endl;
+		cout << "3 * 10 = 30" << endl;
+		return 0;
+	case 4:
+		cout << "4 * 1 = 4" << endl;
+		cout << "4 * 2 = 8" << endl;
+		cout << "4 * 3 = 12" << endl;
+		cout << "4 * 4 = 16" << endl;
+		cout << "4 * 5 = 20" << endl;
+		cout << "4 * 6 = 24" << endl;
+		cout << "4 * 7 = 28" << endl;
+		cout << "4 * 8 = 32" << endl;
+		cout << "4 * 9 = 36" << endl;
+		cout << "4 * 10 = 40" << endl;
+		return 0;
+	case 5:
+		cout << "5 * 1 = 5" << endl;
+		cout << "5 * 2 = 10" << endl;
+		cout << "5 * 3 = 15" << endl;
+		cout << "5 * 4 = 20" << endl;
+		cout << "5 * 5 = 25" << endl;
+		cout << "5 * 6 = 30" << endl;
+		cout << "5 * 7 = 35" << endl;
+		cout << "5 * 8 = 40" << endl;
+		cout << "5 * 9 = 45" << endl;
+		cout << "5 * 1 = 50 " << endl;
+		return 0;
+	case 6:
+		cout << "6 * 1 = 6" << endl;
+		cout << "6 * 2 = 12" << endl;
+		cout << "6 * 3 = 18" << endl;
+		cout << "6 * 4 = 24" << endl;
+		cout << "6 * 5 = 30" << endl;
+		cout << "6 * 6 = 36" << endl;
+		cout << "6 * 7 = 42" << endl;
+		cout << "6 * 8 = 48" << endl;
+		cout << "6 * 9 = 54" << endl;
+		cout << "6 * 10 = 60" << endl;
+		return 0;
+	case 7:
+		cout << "7 * 1 = 7" << endl;
+		cout << "7 * 2 = 14" << endl;
+		cout << "7 * 3 = 21" << endl;
+		cout << "7 * 4 = 28" << endl;
+		cout << "7 * 5 = 35" << endl;
+		cout << "7 * 6 = 42" << endl;
+		cout << "7 * 7 = 49" << endl;
+		cout << "7 * 8 = 56" << endl;
+		cout << "7 * 9 = 63" << endl;
+		cout << "7 * 10 = 70" << endl;
+		return 0;
+	case 8:
+		cout << "8 * 1 = 8" << endl;
+		cout << "8 * 2 = 16" << endl;
+		cout << "8 * 3 = 24" << endl;
+		cout << "8 * 4 = 32" << endl;
+		cout << "8 * 5 = 40" << endl;
+		cout << "8 * 6 = 48" << endl;
+		cout << "8 * 7 = 56" << endl;
+		cout << "8 * 8 = 64" << endl;
+		cout << "8 * 9 = 72" << endl;
+		cout << "8 * 10 = 80" << endl;
+		return 0;
+
+
+	}
+
+
+
+
+
+}
+
+
+
+
+
